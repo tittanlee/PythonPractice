588e931ce6877520333205cebef641e73179337b
1 <1>
2016-01-13 13:37:11 +0800

    add group post test function

--- FB/AutoPost.py
+++ FB/AutoPost.py
@@ -16,25 +16,33 @@ def get_fb_token(app_id, app_secret):
     return result
 
 # oauth_access_token = get_fb_token(FACEBOOK_APP_ID, FACEBOOK_APP_SECRET)
-Tittan_access_token = 'CAACEdEose0cBAPqFvQvxRN3ZAJ9e7AgXCYsMG5hWqS7pgPdhR0OuadySdwimBWezSyVICpMJNOpug6arZCdnxCy0liaPlqw3hmQrryOSid6vZCCdsGb0MPZBZCZCJwopnCyKDpDQasRKE6S4nrwYpva9CzDCQtORKhZCrY0T88ZCiaQpnMVB0kYmWLbWAFPigWpj2PNcQIZCzz9RU98QbXfmF'
-
-oauth_access_token = 'CAACEdEose0cBAIaMVM8MLdFE05cMZB6RC0S6Y0f0FZCX3O0I87I8erKc9L7KqWJK3xdEuhOOzAfKlsxrFmWulSpKLzNBdnV9D6N7KhsilSmZCZAwtMb488sWmWPJxmghpAR3QaYbUmi7nGmwJ3dSxXoekyPipsjNeENoQfXZCpUPPNLC8jZCQiCJKOCkksmhwAab308fmJKiDPRvZClzsGS'
-
+Tittan_access_token = 'CAACEdEose0cBAL7a4YrYHT5IpSHKoPpvT4eLayPxwYOSn5BIfja0kEd3ACdfzt7riZCqcHL71DT6OzuptIkAhIZCGczKvOjXFSg1qnqukRt2ELhm4MhIdXWmeGrmQkC3M3q3MQsvmLSB8V1TB1JFLZBO95oMZCqpkbGPxpX9PsE9io0tmGBYulmj35FKkqg54PbebNaW3X1sbZAVpyOh9'
 
 graph = facebook.GraphAPI(Tittan_access_token, version='2.3')
 me    = graph.get_object("me")
 FbPersonalId = me['id']
 
-GroupsDict= graph.get_object("me/groups")
-for GroupIdx in GroupsDict['data']:
-  print((GroupIdx['name'].decode('utf-8')))
+groups = graph.get_object("me/groups")
+# for GroupIdx in GroupsDict['data']:
+  # print(GroupIdx['name'])
   # print(GroupIdx)
+
+group_id = groups['data'][6]['id'] # we take the ID of the first group
+print(groups['data'][6]['name'])
+print(group_id)
+
+
+#
+# ToDo post message , it is a list include dict{'message':'HERE_IS_YOUR_MSG','link':'YOUR_LINK_URL'}
+# Data = [{'message':'HERE_IS_YOUR_MSG', 'link':'YOUR_LINK_URL'}]
+#
+PostData = [{'message':'Great', 'link':'http://beefun01.com/p/1548/'}]
+# graph.put_object(group_id, "feed", Data)
+graph.put_object(group_id, "feed", data = PostData)
 
 # Likes = graph.get_object("me/likes")
 # for LikeIdx in Likes['data']:
   # print(LikeIdx)
 
-# group_id = groups['data'][0]['id'] # we take the ID of the first group
-# print(group_id)
 
 
