670d6745468ddd42ec3e19ac9b6a14577d31e166
1 <1>
2016-02-15 18:53:20 +0800

    modified the file format as following: msg bee01_fb_link.

--- FB/Beefun01/GetArticle.py
+++ FB/Beefun01/GetArticle.py
@@ -3,11 +3,12 @@ import fbconsole
 import os
 from urllib.parse import urlparse
 
-TOKEN = 'CAACEdEose0cBANulj0uq8mwR5UZCi0nS9xMv5zZC49k0GhBTiZB7EImBrxYAvCSZAdl53px3bLhZCrGCqcIzH9ZBSswLbYuNHEo9oTLwLWZC0H5lFN5OVNZAVKcvYpCZC05A3g6hF77Py8R6qPvIEDKo8phVsBe89EjGqpNuGUEmcpougJQaSl0FRK9yrc8mhLB3sb0pglys56wZDZD'
+TOKEN = 'CAACEdEose0cBAE9XyaMqYMml9HxChfUPtl0vRFKCRIXSBDObpYrm6z0I4EAAplrf0WAU1FKOPCaRPDXgcE3K7bg2JGDIil0TtCRZABcsryqFzvmJUhcMWdZA0kdZC5BuQ3fntykQsrhLaWlDMY0Sx46OwLMLe54sObOVwkZCGz47TeMM5HJBYyH1I8BHwi062xpmYAZC4GgZDZD'
 
 BEEFUN01_GROUP_ID = '425409594316108'
 Be001_Base_Url = 'http://beefun01.com/p/%s/?FB'
-
+Bee01_Url_String = 'http://beefun01.com/p/'
+                   
 
 def write_data_to_file(filename, msg, link):
   directory = './article/'
@@ -19,6 +20,8 @@ def write_data_to_file(filename, msg, link):
   f.write(msg)
   f.write("\n")
   f.write(link)
+  f.write("\n")
+  f.write(" ")
   f.close()
 
 
@@ -40,9 +43,14 @@ for d in fbconsole.iter_pages(Beefun01):
     if (path_name[2].isdigit()):
       file_name = path_name[2]
       bee01_url_fb = Be001_Base_Url %(file_name)
-      print('######   NO.%s   ######'  %idx)
+      print('\n######   NO.%s -- %s --  ######'  %(idx, file_name))
+
+      if Bee01_Url_String in msg:
+        b01_str_idx = msg.find(Bee01_Url_String)
+        msg = msg[0:b01_str_idx]
+      
       print(msg)
-      print(bee01_url_fb + '\n')
+      print(bee01_url_fb)
       write_data_to_file(file_name, msg, bee01_url_fb)
       idx = idx + 1
   except:
