4dea5169031cf99f2d8762975f44bc4a137ca488
1 <1>
2016-02-15 13:57:22 +0800

    1. Changed the write_data_to_file function parameter. 2. separator write msg and link, in order to line by line.

--- FB/Beefun01/GetArticle.py
+++ FB/Beefun01/GetArticle.py
@@ -3,21 +3,25 @@ import fbconsole
 import os
 from urllib.parse import urlparse
 
-TOKEN = 'CAACEdEose0cBAEJxayTM6S0szjQvbkcr5CU8GjESay51feLPlIoXykrhfQJ28nYGUrQC4q3XEaYZCds5ZCzmvlATFrMjo1tJOZA0Dss190ly9MaP4hYkgnXApZA3aSCVvd5JT2cMyhMt8ZC2fmsZCa1k6hJ8v1xhRpG1b2yt5Lp3K7HNTXJWSAhLjC3ZCb3Dwp0U0e5ledJWt9FjWEKcvt3'
+TOKEN = 'CAACEdEose0cBANulj0uq8mwR5UZCi0nS9xMv5zZC49k0GhBTiZB7EImBrxYAvCSZAdl53px3bLhZCrGCqcIzH9ZBSswLbYuNHEo9oTLwLWZC0H5lFN5OVNZAVKcvYpCZC05A3g6hF77Py8R6qPvIEDKo8phVsBe89EjGqpNuGUEmcpougJQaSl0FRK9yrc8mhLB3sb0pglys56wZDZD'
 
 BEEFUN01_GROUP_ID = '425409594316108'
 Be001_Base_Url = 'http://beefun01.com/p/%s/?FB'
 
 
-def write_data_to_file(filename, data):
-  directory = './files'
+def write_data_to_file(filename, msg, link):
+  directory = './article/'
   if not os.path.exists(directory):
     os.makedirs(directory)
 
-  filepath = './files/' + filename + '.txt'
-  f = open(filepath, 'w+', encoding = 'utf-8')
-  f.write(data)
+  filepath = directory + filename + '.txt'
+  f = open(filepath, 'w', encoding = 'utf-8')
+  f.write(msg)
+  f.write("\n")
+  f.write(link)
   f.close()
+
+
 
 
 graph = facebook.GraphAPI(TOKEN, version="2.3")
@@ -39,7 +43,7 @@ for d in fbconsole.iter_pages(Beefun01):
       print('######   NO.%s   ######'  %idx)
       print(msg)
       print(bee01_url_fb + '\n')
-      write_data_to_file(file_name, msg + bee01_url_fb)
+      write_data_to_file(file_name, msg, bee01_url_fb)
       idx = idx + 1
   except:
       print('#####  End  #####')
